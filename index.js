f5428e6a6f96defa1a2c302937e03e15ba03b4b2
Mahtab Mustakim <mtbbsl@gmail.com>
2023-06-12 01:03:45 +0600

    basic server initialized

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("DesignTechIT server is running");
+});
+
+app.listen(port, () => {
+  console.log(`DesignTechIT is running on port ${port}`);
+});
