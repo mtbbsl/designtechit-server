7a00b458edda46d30c9ab92d5e12e2536a45abfc
Mahtab Mustakim <mtbbsl@gmail.com>
2023-06-13 01:58:39 +0600

    Cart Collection api get updated

--- index.js
+++ index.js
@@ -41,8 +41,18 @@ async function run() {
       res.send(result);
     });
 
-    // Cart collection
-    app.post('/carts', async(req, res) => {
+    // Cart collection apis
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post("/carts", async(req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
