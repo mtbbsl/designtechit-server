5fcad4bb32fb7e6a484792edd35c3f6f960326de
Mahtab Mustakim <mtbbsl@gmail.com>
2023-06-12 20:22:30 +0600

    omit await from client.connect()

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rsspdr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,15 +23,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const classCollection = client.db("designtechitDB").collection("classes");
     const userCollection = client.db("designtechitDB").collection("users");
 
-    app.get('/classes', async(req, res) => {
-        const result = await classCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
@@ -50,7 +49,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("DesignTechIT server is running");
